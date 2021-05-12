9ef119aeffeb48935c44d88fef8a2aa882f6f5ef
root <root@tangled.ae>
2021-05-12 12:16:53 +0000

    Removed some comments

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-    $link = mysqli_connect("localhost", "neeltron", "********", "neeltron");
+	**creds censored**
 	if (!$link) {
   		die("Connection failed: " . mysqli_connect_error());
 	}
@@ -25,15 +25,9 @@
   <title>RoboNet</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
-
-  <!-- Favicons -->
   <link href="assets/img/favicon.png" rel="icon">
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
-
-  <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Jost:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
   <link href="assets/vendor/aos/aos.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
@@ -41,28 +35,15 @@
   <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
   <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
   <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-
-  <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
-
-  <!-- =======================================================
-  * Template Name: Arsha - v4.1.0
-  * Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 </head>
 
 <body>
 
-  <!-- ======= Header ======= -->
   <header id="header" class="fixed-top ">
     <div class="container d-flex align-items-center">
 
       <h1 class="logo me-auto"><a href="index.html">RoboNet</a></h1>
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-
       <nav id="navbar" class="navbar">
         <ul>
           <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
@@ -70,35 +51,13 @@
           <li><a class="nav-link scrollto" href="#services">Register</a></li>
           <li><a class="nav-link   scrollto" href="#portfolio">Login</a></li>
           <li><a class="nav-link scrollto" href="#team">Contributors</a></li>
-<!--
-          <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li>
-          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
--->
           <li><a class="getstarted scrollto" href="#about">Get Started</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav><!-- .navbar -->
+      </nav>
 
     </div>
-  </header><!-- End Header -->
-
-  <!-- ======= Hero Section ======= -->
+  </header>
   <section id="hero" class="d-flex align-items-center">
 
     <div class="container">
@@ -117,11 +76,9 @@
       </div>
     </div>
 
-  </section><!-- End Hero -->
+  </section>
 
   <main id="main">
-
-    <!-- ======= Cliens Section ======= -->
     <section id="cliens" class="cliens section-bg">
       <div class="container">
 
@@ -154,9 +111,7 @@
         </div>
 
       </div>
-    </section><!-- End Cliens Section -->
-
-    <!-- ======= About Us Section ======= -->
+    </section>
     <section id="about" class="about">
       <div class="container" data-aos="fade-up">
 
@@ -187,9 +142,7 @@
         </div>
 
       </div>
-    </section><!-- End About Us Section -->
-
-    <!-- ======= Why Us Section ======= -->
+    </section>
     <section id="why-us" class="why-us section-bg">
       <div class="container-fluid" data-aos="fade-up">
 
@@ -242,9 +195,7 @@
         </div>
 
       </div>
-    </section><!-- End Why Us Section -->
-
-    <!-- ======= Skills Section ======= -->
+    </section>
     <section id="skills" class="skills">
       <div class="container" data-aos="fade-up">
 
@@ -295,9 +246,7 @@
         </div>
 
       </div>
-    </section><!-- End Skills Section -->
-
-    <!-- ======= Services Section ======= -->
+    </section>
     <section id="services" class="services section-bg">
       <div class="container" data-aos="fade-up">
 
@@ -359,9 +308,7 @@
         </div>
 
       </div>
-    </section><!-- End Cta Section -->
-
-    <!-- ======= Portfolio Section ======= -->
+    </section>
     <section id="portfolio" class="portfolio">
       <div class="container" data-aos="fade-up">
 
@@ -472,9 +419,7 @@
         </div>
 
       </div>
-    </section><!-- End Portfolio Section -->
-
-    <!-- ======= Team Section ======= -->
+    </section>
     <section id="team" class="team section-bg">
       <div class="container" data-aos="fade-up">
 
@@ -556,9 +501,7 @@
         </div>
 
       </div>
-    </section><!-- End Team Section -->
-
-    <!-- ======= Pricing Section ======= -->
+    </section>
     <section id="pricing" class="pricing">
       <div class="container" data-aos="fade-up">
 
@@ -617,9 +560,7 @@
         </div>
 
       </div>
-    </section><!-- End Pricing Section -->
-
-    <!-- ======= Frequently Asked Questions Section ======= -->
+    </section>
     <section id="faq" class="faq section-bg">
       <div class="container" data-aos="fade-up">
 
@@ -679,9 +620,7 @@
         </div>
 
       </div>
-    </section><!-- End Frequently Asked Questions Section -->
-
-    <!-- ======= Contact Section ======= -->
+    </section>
     <section id="contact" class="contact">
       <div class="container" data-aos="fade-up">
 
@@ -749,11 +688,9 @@
         </div>
 
       </div>
-    </section><!-- End Contact Section -->
+    </section>
 
-  </main><!-- End #main -->
-
-  <!-- ======= Footer ======= -->
+  </main>
   <footer id="footer">
 
     <div class="footer-newsletter">
@@ -828,19 +765,13 @@
         &copy; Copyright <strong><span>Arsha</span></strong>. All Rights Reserved
       </div>
       <div class="credits">
-        <!-- All the links in the footer should remain intact. -->
-        <!-- You can delete the links only if you purchased the pro version. -->
-        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/ -->
         Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
       </div>
     </div>
-  </footer><!-- End Footer -->
+  </footer>
 
   <div id="preloader"></div>
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-  <!-- Vendor JS Files -->
   <script src="assets/vendor/aos/aos.js"></script>
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
@@ -848,8 +779,6 @@
   <script src="assets/vendor/php-email-form/validate.js"></script>
   <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
   <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
-
-  <!-- Template Main JS File -->
   <script src="assets/js/main.js"></script>
 
 </body>
